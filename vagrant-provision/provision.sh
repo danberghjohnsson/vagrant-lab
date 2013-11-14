80b65019ec62c4d3ee171375c656f9b1b5081fd8
Dan Bergh Johnsson <dan.bergh.johnsson@gmail.com>
2013-11-14 14:47:39 +0100

    Datafil basedata.txt flyttad till underbibliotek data

--- vagrant-provision/provision.sh
+++ vagrant-provision/provision.sh
@@ -3,5 +3,5 @@ apt-get update
 echo "Installing PostgreSQL"
 apt-get --assume-yes install postgresql
 echo "Provisioning basedata"
-cat /vagrant/basedata.txt
+cat /vagrant/data/basedata.txt
 echo "Provision finished"
