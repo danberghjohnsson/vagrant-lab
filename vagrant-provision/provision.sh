7e7b50ec0289fa9b424f00e94da09cce11fe0a5c
Dan Bergh Johnsson <dan.bergh.johnsson@gmail.com>
2013-11-14 14:43:26 +0100

    Provision av testdata i script-exepmplet

--- vagrant-provision/provision.sh
+++ vagrant-provision/provision.sh
@@ -1,2 +1,7 @@
+echo "Provision started"
 apt-get update
+echo "Installing PostgreSQL"
 apt-get --assume-yes install postgresql
+echo "Provisioning basedata"
+cat /vagrant/basedata.txt
+echo "Provision finished"
